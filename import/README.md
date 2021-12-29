2b95443b4e507ae3e8146b7588428c90c1fd93d6
Elliot Chance <elliotchance@gmail.com>
2021-12-29 00:53:57 -0500

    Import from Apple Music
    Importing a tracklist from Apple Music is as easy as pasting the album
    URL into a new "Import From..." model. I have started with Apple
    Music, but I will add more soon.
    
    The Tracklist Editor still works entirely offline if you don't use the
    import feature. However, due to CORS the import feature cannot be part
    of the static web page. I will have to manually push changes to the
    importer. If that becomes a pain I can automate it with CD later.
    
    One known issue: If the album is semi-various artists, the importer will
    only catch a few tracks. Semi-various artists is a thing with
    Apple Music where albums that contain mostly one artist do not show that
    artist on those tracks. This causes havoc with the (probably brittle)
    regex that parses the raw HTML.

---
+++ import/README.md
@@ -0,0 +1,22 @@
+Some of these instructions are mostly for my own benefit, but if you wanted to
+deploy your own copy for debugging or any other reason, go for it.
+
+# Run Tests
+
+```sh
+make test
+```
+
+# Deploy
+
+Development:
+
+```bash
+make dev
+```
+
+Production (will run tests first):
+
+```bash
+make prod
+```
---
+++ import/apple-music.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Examples:
+//   sls invoke -f apple-music -d '{"url":"https://music.apple.com/us/album/nye-2022-dj-mix/1600990821"}'
+//   curl "https://sxxqp2lco7.execute-api.us-east-1.amazonaws.com/dev/apple-music?url=https%3A%2F%2Fmusic.apple.com%2Fus%2Falbum%2Fnye-2022-dj-mix%2F1600990821"
+
+const fetch = require('node-fetch');
+
+module.exports.import = async (event) => {
+  // Validate the URL to prevent mistakes and abuse.
+  const querystring = event.queryStringParameters;
+  const url = querystring.url;
+  if (!url.startsWith('https://music.apple.com/us/album/')) {
+    return response(400, {
+      error: `Invalid URL: ${url}`,
+    });
+  }
+
+  // Fetch the raw HTML.
+  const resp = await fetch(url);
+  const body = await resp.text();
+
+  // Parse tracks.
+  //
+  // TODO(elliotchance): This is an ultra crude regexp that will probably break
+  //  in the future.
+  //
+  // There are a few layouts we need to test, specially various artist albums
+  // have a different layout than a single artist album.
+
+  // If we get more than one match, this must be a VA album. The regexp happens
+  // to fall out that way, but also an album with one track could hardly be
+  // considered various artists. Maybe this is possible - I haven't seen any
+  // examples of this though.
+  let matches = Array.from(body.matchAll(/songs-list-row__song-name">([^<]+).*?row__link".*?>([^<]+).*?row__length">([^<]+)/gs));
+  let decoder = (match, number) => ({
+    number,
+    title: match[2].trim() + ' - ' + match[1].trim(),
+    time: match[3].trim(),
+  });
+
+  if (matches.length < 2) {
+    matches = Array.from(body.matchAll(/songs-list-row__song-name".*?>(.*?)<.*?row__length".*?>(.*?)</gs));
+    decoder = (match, number) => ({
+      number,
+      title: match[1].trim(),
+      time: match[2].trim(),
+    });
+  }
+
+  let tracks = [];
+  let number = 1;
+  for (const match of matches) {
+    tracks.push(decoder(match, number));
+    ++number;
+  }
+
+  return response(200, {
+    tracks,
+  });
+};
+
+function response(statusCode, body) {
+  return {
+    statusCode,
+    body: JSON.stringify(body),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  };
+}
